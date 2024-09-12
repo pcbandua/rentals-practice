645ae4b7d59bfec43a85568d6ad26409677a158a
Paulo Bandua <pcbandua@gmail.com>
2024-09-12 12:39:46 -0700

    create action added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,20 @@
 class HomeController < ApplicationController
+  def index
+    @homes = Home.all
+    render :index
+  end
+
+  def create
+    @home = Home.create(
+      description: params[:description],
+      year_built: params[year_built],
+      square_feet: params[year_built],
+      bedrooms: params[bedrooms],
+      floors: params[floors],
+      availability: params[availability],
+      address: params[address],
+      price: params[price],
+    )
+    render :show
+  end
 end
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -4,4 +4,30 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test "index" do
+    get "/homes.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal Home.count, data.length
+  end
+
+  test "create" do
+    assert_difference "Home.count", 1 do
+      post "/homes.json", params: { description: "big house", year_built: 1999, square_feet: 4230, bedrooms: 3, floors: "carpet", availability: "sold", address: "450 deng ave, Nashville, TN", price: 450000 }
+    end
+  end
+
+  # create_table "homes", force: :cascade do |t|
+  #   t.string "description"
+  #   t.integer "year_built"
+  #   t.integer "square_feet"
+  #   t.integer "bedrooms"
+  #   t.integer "bathrooms"
+  #   t.string "floors"
+  #   t.string "availability"
+  #   t.string "address"
+  #   t.integer "price"
+  #   t.datetime "created_at", null: false
+  #   t.datetime "updated_at", null: false
 end
